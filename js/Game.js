7c8443a382a9963f3ebc5e46fd3af12d06f1f882
AafreenKhan040896 <77886222+AafreenKhan040896@users.noreply.github.com>
2022-05-18 07:43:22 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,26 @@
+class Game {
+  constructor() {
+  }
+
+  getState(){
+
+  }
+
+  update(state){
+
+  }
+
+  start(){
+
+  }
+
+  handleElements(){
+    form.hide();
+    form.titleImg.position(40, 50);
+    form.titleImg.class("gameTitleAfterEffect");
+  }
+
+  play(){
+
+  }
+}
